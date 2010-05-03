a742751418aacf4462b4abfd502d29c9bbc006db
Bruno Caricchio Buss <bruno.buss@gmail.com>
2010-05-03 10:37:07 -0300

    Corrigindo um pequeno probleminha de espaços VS tab.

--- src/main.c
+++ src/main.c
@@ -9,10 +9,10 @@ int redesenhaJanela = 1;
 
 int main(int argc, char *argv[])
 {
-        setupGlut(&argc, argv);
-        glutMainLoop();
+	setupGlut(&argc, argv);
+	glutMainLoop();
 
-        return 0;
+	return 0;
 }
 
 void setupGlut(int *argc, char *argv[])
@@ -20,19 +20,19 @@ void setupGlut(int *argc, char *argv[])
 
 	DBG(printf("Configurando o GLUT...\n"));
 
-        glutInit(argc, argv);
-        glutInitDisplayMode(CONFIG_DISPLAYMODE);
-        glutInitWindowSize(tamJanelaX, tamJanelaY);
-        glutInitWindowPosition(CONFIG_POSX_INICIAL, CONFIG_POSY_INICIAL);
-        glutCreateWindow(CONFIG_TITULO_JANELA);
+	glutInit(argc, argv);
+	glutInitDisplayMode(CONFIG_DISPLAYMODE);
+	glutInitWindowSize(tamJanelaX, tamJanelaY);
+	glutInitWindowPosition(CONFIG_POSX_INICIAL, CONFIG_POSY_INICIAL);
+	glutCreateWindow(CONFIG_TITULO_JANELA);
 
-        glClearColor(0.0, 0.0, 0.0, 0.0); /* Define a cor do GL_COLOR_BUFFER_BIT */
-        glShadeModel(GL_FLAT);
+	glClearColor(0.0, 0.0, 0.0, 0.0); /* Define a cor do GL_COLOR_BUFFER_BIT */
+	glShadeModel(GL_FLAT);
 
-        glutDisplayFunc(displayCallBack);
-        glutReshapeFunc(reshapeCallBack);
+	glutDisplayFunc(displayCallBack);
+	glutReshapeFunc(reshapeCallBack);
 	glutKeyboardFunc(tecladoCallBack);
-        glutMouseFunc(mouseCallBack);
+	glutMouseFunc(mouseCallBack);
 	glutIdleFunc(idleCallBack);
 	glutTimerFunc(CONFIG_FPS_TIMER, timerFPS, 1);
 
@@ -44,14 +44,13 @@ void displayCallBack(void)
 	if(redesenhaJanela == 0) return;
 
 	DBG(printf("Redesenhando a tela...\n"));
+	redesenhaJanela = 0;
 
-        glClear(GL_COLOR_BUFFER_BIT); /* "Apagando" a tela */
+	glClear(GL_COLOR_BUFFER_BIT); /* "Apagando" a tela */
 
-        glutSwapBuffers();
+	glutSwapBuffers();
 
 	DBG(printf("... terminou o redesenho.\n"));
-
-	redesenhaJanela = 0;
 }
 
 void idleCallBack()
@@ -67,15 +66,15 @@ void mouseCallBack(int botao, int estado, int x, int y)
 {
 	DBG(printf("Acao do mouse detectada: botao = %d estado = %d x = %d u = %d\n", botao, estado, x, y));
 
-        switch (botao)
-        {
-                case GLUT_LEFT_BUTTON:
-                        break;
-                case GLUT_MIDDLE_BUTTON:
-                        break;
+	switch (botao)
+	{
+		case GLUT_LEFT_BUTTON:
+			break;
+		case GLUT_MIDDLE_BUTTON:
+			break;
 		case GLUT_RIGHT_BUTTON:
 			break;
-                default: break;
+		default: break;
         }
 }
 
@@ -83,14 +82,13 @@ void reshapeCallBack(int w, int h)
 {
 	DBG(printf("Redimensionando a janela para: largura = %d altura = %d\n", w, h));
 
+	glViewport(0, 0, (GLsizei) w, (GLsizei) h);
+	glMatrixMode(GL_PROJECTION);
 
-        glViewport(0, 0, (GLsizei) w, (GLsizei) h);
-        glMatrixMode(GL_PROJECTION);
-
-        glLoadIdentity();
-        glOrtho(-50.0, 50.0, -50.0, 50.0, -1.0, 1.0);
-        glMatrixMode(GL_MODELVIEW);
-        glLoadIdentity();
+	glLoadIdentity();
+	glOrtho(-50.0, 50.0, -50.0, 50.0, -1.0, 1.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
 }
 
 void timerFPS(int valor)
@@ -98,6 +96,6 @@ void timerFPS(int valor)
 	DBG(printf("Temporizador disparado.\n"));
 
 	redesenhaJanela = 1;
-        glutPostRedisplay();
+	glutPostRedisplay();
 	glutTimerFunc(CONFIG_FPS_TIMER, timerFPS, 1);
 }
