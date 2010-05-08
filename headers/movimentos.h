c24fbac26e41e5f349668317ae853e5ad5f008d9
Bruno Caricchio Buss <bruno.buss@gmail.com>
2010-05-08 17:09:15 -0300

    Headers devem ter a proteção contra inclusão múltipla.

--- headers/movimentos.h
+++ headers/movimentos.h
@@ -1,5 +1,10 @@
+#ifndef H_MOVIMENTOS
+#define H_MOVIMENTOS
+
 #include <GL/glut.h>
 #include <stdio.h>
 #include "debug.h"
 
 void movimentos(GLdouble l, GLfloat a, GLfloat d, GLfloat t);
+
+#endif
